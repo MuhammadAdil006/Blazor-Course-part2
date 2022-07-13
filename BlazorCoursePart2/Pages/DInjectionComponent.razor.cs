02b1fd4a5af68006ff7dfbea91455daaa2b07fd9
MuhammadAdil006 <qadil392@gmail.com>
2022-07-13 17:52:13 +0500

    dependancy injection

---
+++ BlazorCoursePart2/Pages/DInjectionComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿using BlazorCoursePart2.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorCoursePart2.Pages
+{
+    public partial class DInjectionComponent
+    {
+      [Inject]
+      IEmployee MyEmployee { get; set; }
+    List<Employee> list;
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            list = MyEmployee.GetEmployees();
+        }
+    }
+}
